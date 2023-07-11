dc46ff8274ae04058b7f42c435b64a9d16f8ba72
IKalonji <IKalonji@student.wethinkcode.co.za>
2023-07-11 09:00:30 +0200

    Added addtional functionality to the server, scaffold the frontend

--- SERVER/commands/command_handler.py
+++ SERVER/commands/command_handler.py
@@ -2,15 +2,13 @@ import json
 import subprocess
 import os
 import time
+import pexpect
 
 from commands.response_codes import Response_Codes
 
 class Command_Handler(Response_Codes):
     def __init__(self) -> None:
         super().__init__()
-        process = subprocess.Popen(['pwd'],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        print(stdout, stderr)
         self.valid_commands = {
             "create_workspace": self.create_workspace,
             "delete_workspace": self.delete_workspace,
@@ -27,11 +25,13 @@ class Command_Handler(Response_Codes):
         }
         self.BASE_DIR = "users"
         self.invalid_command_response = json.dumps({"result": self.ERROR, "detail": self.invalid_command})
+        self.WALLET_CREATOR_ACCOUNT = "0x7721b98bbf12fcb9"
+        self.WALLET_CREATOR_KEY = "4d2834338c2a35aca39ab8be94b45fc5d5722975a1114e0f3dac80ee32813e5e"
 
-    def handle_request(self, request) -> json:
+    def handle_request(self, request):
         try:
             execution_response = self.valid_commands[request["command"]](request)
-            return json.dumps(execution_response)
+            return execution_response
         except KeyError as invalid_command:
             return self.invalid_command_response
     
@@ -67,35 +67,41 @@ class Command_Handler(Response_Codes):
         except KeyError as missing_args:
             return self.invalid_command_response
 
-    def get_workspace(self, parameters): ##TODO
-        '''
-        [
-            {
-                workspace: name,
-                folders: {
-                        "contracts": [
-                            {
-                                name: name,
-                                content: content
-                            },
-                        ]
-                    }
-                },
-                flow.json: "content",
-                readme: "content"
-            }
-        ]
-        '''
+    def get_workspace(self, parameters):
         try:
             user = parameters["user"]
+            users_folder_path = os.path.join(os.getcwd(),"users", user)
+            workspace_builder = []
+            for workspace in os.listdir(users_folder_path):
+                new_workspace = {
+                    "workspace": workspace,
+                    "folders": {
 
-            workspace = ""
-            contracts = []
-            transactions = []
-            scripts = []
-            tests = []
-
-            return {"result": self.codes.SUCCESS, "detail": "command executed successfully"}
+                    },
+                    "flow.json": "",
+                    "README.md": ""
+                }
+                for file_or_folder in os.listdir(os.path.join(users_folder_path, workspace)):
+                    if os.path.isfile(os.path.join(users_folder_path, workspace, file_or_folder)) :
+                        with open(os.path.join(users_folder_path, workspace, file_or_folder), "r", encoding='utf8') as file:
+                            file_content = file.readlines()
+                            new_workspace[file_or_folder] = "\n".join(file_content)
+                    elif file_or_folder == "cadence":
+                        for folder in os.listdir(os.path.join(users_folder_path, workspace, "cadence")):
+                            new_workspace["folders"][folder] = []
+                            for file_in_folder in os.listdir(os.path.join(users_folder_path, workspace, "cadence", folder)):
+                                if file_in_folder[0] == ".":
+                                    continue
+                                with open(os.path.join(users_folder_path, workspace, "cadence", folder, file_in_folder), "r", encoding='utf8') as file:
+                                    file_content = file.readlines()
+                                    new_workspace["folders"][folder].append(
+                                        {
+                                            "name": file_in_folder,
+                                            "content": "\n".join(file_content)
+                                        }
+                                    )
+                    workspace_builder.append(new_workspace)
+            return {"result": self.SUCCESS, "detail": "command executed successfully", "data": workspace_builder}
         except KeyError as missing_args:
             return self.invalid_command_response
 
@@ -136,9 +142,9 @@ class Command_Handler(Response_Codes):
         except KeyError as missing_args:
             return self.invalid_command_response
 
-    def deploy_contracts(self, parameters): #TODO
+    def deploy_contracts(self, parameters):
         try:
-            user, workspace, account, network, file = parameters["user"], parameters["workspace"], parameters["account"], parameters["network"], parameters["file"]
+            user, workspace, account, network, file = parameters["user"], parameters["workspace"], parameters["account_name"], parameters["network"], parameters["file"]
             path_to_contract = os.path.join("./","cadence", "contracts", file)
             command = f"cd {self.BASE_DIR} && cd {user} && cd {workspace} && flow accounts add-contract {path_to_contract} --network {network} --signer {account}"
             response = self.execute_shell_command(command=command)
@@ -151,26 +157,71 @@ class Command_Handler(Response_Codes):
             user, workspace, account, network = parameters["user"], parameters["workspace"], parameters["account"], parameters["network"]
             return {"result": self.codes.SUCCESS, "detail": "command executed successfully"}
         except KeyError as missing_args:
-            
             return self.invalid_command_response
         
-    def create_account(self, parameters): #TODO
+    def create_account(self, parameters):
         try:
-            return {"result": self.codes.SUCCESS, "detail": "command executed successfully"}
+            user, workspace, account_name = parameters["user"], parameters["workspace"], parameters["account_name"]
+            command = f"cd {self.BASE_DIR} && cd {user} && cd {workspace} && flow keys generate --output json --save ./keys.json"
+            response = self.execute_shell_command(command=command)
+            print("First command response: ", response)
+            if response["result"] == self.SUCCESS:
+                with open(os.path.join(os.getcwd(), self.BASE_DIR, user, workspace, "flow.json"), "r", encoding='utf8') as file:
+                    flow_config = json.load(file)
+                flow_config["accounts"]["wallet-creator-account"] = {
+                    "address": self.WALLET_CREATOR_ACCOUNT,
+                    "key": self.WALLET_CREATOR_KEY
+                }
+                with open(os.path.join(os.getcwd(), self.BASE_DIR, user, workspace, "flow.json"), "w", encoding='utf8') as file:
+                    json_obj = json.dumps(flow_config, indent=4)
+                    file.write(json_obj)
+                    file.close()
+                    with open(os.path.join(os.getcwd(), self.BASE_DIR, user, workspace, "keys.json"), "r", encoding='utf8') as keys_file:
+                        generated_keys = json.load(keys_file)
+                        pub_key = generated_keys.get("public")
+                        priv_key = generated_keys.get("private")
+                        command = f"cd {self.BASE_DIR} && cd {user} && cd {workspace} && flow accounts create --key {pub_key} --signer wallet-creator-account --network testnet --config-path flow.json --output json --save new-wallet.json"
+                        response = self.execute_shell_command(command=command)
+                        if response["result"] == self.SUCCESS:
+                            with open(os.path.join(os.getcwd(), self.BASE_DIR, user, workspace, "new-wallet.json"), "r", encoding='utf8') as new_wallet_file:
+                                new_wallet = json.load(new_wallet_file)
+                                address = new_wallet.get("address")
+                                del flow_config["accounts"]["wallet-creator-account"]
+                                flow_config["accounts"][account_name] = {
+                                    "address": address,
+                                    "key": priv_key
+                                }
+                                with open(os.path.join(os.getcwd(), self.BASE_DIR, user, workspace, "flow.json"), "w", encoding='utf8') as file:
+                                    json_obj = json.dumps(flow_config, indent=4)
+                                    file.write(json_obj)
+                                    file.close()
+                                response["data"] = {
+                                    "address": address,
+                                    "pubkey": pub_key,
+                                    "privkey": priv_key
+                                }
+                                new_wallet_file.close()
+                        keys_file.close()
+            self.execute_shell_command(f"cd {self.BASE_DIR} && cd {user} && cd {workspace} && rm -rf ./keys.json")
+            self.execute_shell_command(f"cd {self.BASE_DIR} && cd {user} && cd {workspace} && rm -rf ./new-wallet.json")
+            return response
         except KeyError as missing_args:
             return self.invalid_command_response
             
     def run_script(self, parameters): #TODO
         try:
-            user, workspace, account, network = parameters["user"], parameters["workspace"], parameters["account"], parameters["network"]
-            return {"result": self.codes.SUCCESS, "detail": "command executed successfully"}
+            user, workspace, network, file = parameters["user"], parameters["workspace"], parameters["network"], parameters["file"]
+            path_to_script = os.path.join("./","cadence", "scripts", file)
+            command = f"cd {self.BASE_DIR} && cd {user} && cd {workspace} && flow scripts execute {path_to_script} --network {network}"
+            response = self.execute_shell_command(command=command)
+            return response
         except KeyError as missing_args:
             return self.invalid_command_response
     
     def execute_shell_command(self, command):
-        print("Command: ", command)
-        print("DIR: ", os.getcwd())
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
         stdout, stderr = process.communicate()
-        return {"result": self.SUCCESS if not stderr else self.ERROR, "detail": stdout.decode(), "error": stderr.decode()}
+        return {"result": self.SUCCESS if not stderr else self.ERROR, "detail": stdout, "error": stderr}
+
+        
 
