972612f357d1d7568ccb9faefcca9fb27e73290f
IKalonji <IKalonji@student.wethinkcode.co.za>
2023-07-15 19:13:08 +0200

    Fixed issues with Editor

--- SERVER/app.py
+++ SERVER/app.py
@@ -1,12 +1,17 @@
+import os
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 from logging import log
-
+from dotenv import dotenv_values, load_dotenv
 from commands.command_handler import Command_Handler
+import openai
 
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
 handler = Command_Handler()
+print(os.getenv('OPENAI'))
+openai.api_key = os.getenv('OPENAI')
 
 BASE_URL = "/v1/flowde/"
 
@@ -26,6 +31,18 @@ def live():
 def command(command):
   request_data = request.get_json()
   return handler.handle_request(command,dict(request_data))
+
+@app.route(f"/v1/ricardian", methods=["POST"])
+def ricardian():
+  request_data = request.get_json()
+  # create a chat completion
+  chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[request_data])
+  # print the chat completion
+  response = chat_completion.choices[0].message.content
+  if response:
+    return {"result": "OK", "response": response}
+  else:
+    return {"result": "ERROR", "response": "ERROR in the response"}
 
 if __name__ == "__main__":
   app.run(port=5555, host="0.0.0.0", debug=True)
